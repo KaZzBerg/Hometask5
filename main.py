2e47226a03744f6da314001abd3871caf5ce6c03
Vlad Raykov <vladraykov48@gmail.com>
2023-07-25 18:12:20 +0300

    task2

--- main.py
+++ main.py
@@ -15,3 +15,25 @@ print('Sum of even Positive numbers:', sum(y))
 print('Sum odd numbers:', sum(z))
 print(f'Sum of every third number:', sum(numbers[2:15:3]))
 print(f'Multiplication of max and min values:', max(numbers) * min(numbers))
+try:
+    s1 = s2 = 0
+    for s1, a in enumerate(numbers):
+        if a > 0:
+            break
+    for s2, a in enumerate(reversed(numbers)):
+        if a > 0:
+            break
+    s = sum(numbers[s1 + 1: -s2 - 1])
+    print(f'Sum of numbers between:', {s})
+except ValueError as e:
+    print(f'Error: {e}')
+
+    print('No positive numbers')
+
+    print('No positive numbers')
+
+print(y)
+print(z)
+print(x)
+h = [i for i in numbers if i > 0]
+print(h)
